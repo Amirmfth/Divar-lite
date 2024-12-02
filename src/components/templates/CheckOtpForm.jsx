dde0ff16a4c62bc5bae327e8a30661c7b2a5776e
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-02 17:52:03 +0330

    complete SendOtp comp

---
+++ src/components/templates/CheckOtpForm.jsx
@@ -0,0 +1,10 @@
+
+function CheckOtpForm() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckOtpForm
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,23 @@
+// hooks
+import { useState } from "react";
+// components
+import CheckOtpForm from "../components/templates/CheckOtpForm";
+import SendOtpForm from "../components/templates/SendOtpForm";
+
+function AuthPage() {
+  // states
+  const [step, setStep] = useState(1);
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+
+  return (
+    <div>
+      {step === 1 && (
+        <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />
+      )}
+      {step === 2 && <CheckOtpForm />}
+    </div>
+  );
+}
+
+export default AuthPage;
