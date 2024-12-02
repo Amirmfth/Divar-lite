57620dda689ab544c0eb7f77d1214228bcb04eb5
Amirmfth <102855219+Amirmfth@users.noreply.github.com>
2024-12-02 18:39:20 +0330

    complete CheckOtp comp

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,10 +1,33 @@
+import { checkOtp } from "../../services/auth";
+import {setCookies} from "../../utils/cookie";
 
-function CheckOtpForm() {
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  // handlers
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (code.length !== 5) return;
+    const { response, error } = await checkOtp(mobile, code);
+    if (response) {
+        setCookies(response.data)
+    }
+    if (error) console.log(error.response.data.message);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>تایید کد اس ام اس شده</p>
+      <span>کد پیامک شده به شماره "{mobile}" را وارد کنید</span>
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 }
 
-export default CheckOtpForm
+export default CheckOtpForm;
